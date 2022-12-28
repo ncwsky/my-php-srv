f5e8d0db31890df785e4ed0915a9b249fb0c5c6b
ep1088 <ep1088@qq.com>
2022-12-28 17:00:05 +0800

    namespace myphp

--- srv_demo/SrvDemo2.php
+++ srv_demo/SrvDemo2.php
@@ -3,7 +3,7 @@
 $_SERVER['SCRIPT_FILENAME'] = __FILE__; //重置运行
 define('APP_PATH', __DIR__ .'/app');
 
-$cfg = require(__DIR__ . '/wokerman.conf.php');
+$cfg = require(__DIR__ . '/wokerman2.conf.php');
 #require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/vendor/workerman/workerman/Autoloader.php';
 require __DIR__ . '/vendor/myphps/my-php-srv/Load.php';
